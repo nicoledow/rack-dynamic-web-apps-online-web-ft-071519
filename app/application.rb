3827de6cec9513b41d941b6c15e2476dc4ef817b
Nicole Dow <nikki.dow71@gmail.com>
2019-08-22 14:24:44 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,6 +8,12 @@ class Application
    num_3 = Kernel.rand(1..20)
    
    if num_1 == num_2 && num_2 == num_3
+     resp.write "You win"
+    else 
+      resp.write "You lose"
+    end
+    
+    resp.finish
    
   end
 
